7a753b74180fe8e8be922900c8ecbd028582a1db
Daniel Husar <dano.husar@gmail.com>
2018-12-01 14:27:07 -0800

    1

---
+++ 1/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString();
+
+const calculate = (input, initialFrequency) => {
+  const frequencies = [];
+  let nonUniqFrequency = null;
+
+  const getSum = initialFrequency =>
+    input
+      .split('\n')
+      .map(Number)
+      .filter(Boolean)
+      .reduce(reducer, initialFrequency);
+
+  const reducer = (accumulator, currentValue) => {
+    if (!nonUniqFrequency) {
+      frequencies.includes(accumulator)
+        ? (nonUniqFrequency = accumulator)
+        : frequencies.push(accumulator);
+    }
+    return accumulator + currentValue;
+  };
+
+  let firstIteration = (iteration = getSum(0));
+  while (nonUniqFrequency == null) {
+    iteration = getSum(iteration);
+  }
+
+  return [firstIteration, nonUniqFrequency];
+};
+
+const start = Date.now();
+const results = calculate(input, 0);
+const benchmark = Math.round((Date.now() - start) / 1000);
+
+console.log(results);
+console.log(`Tests pass: ${results[0] === 578 && results[1] === 82516}`);
+console.log(`Took: ${benchmark} seconds`);
