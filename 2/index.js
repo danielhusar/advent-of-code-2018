d2633fb0796056d42fc44d103609b39b0a15d8ab
Daniel Husar <dano.husar@gmail.com>
2018-12-02 12:34:22 -0800

    Day 2 part 2

--- 2/index.js
+++ 2/index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString();
 const data = input.split('\n').filter(Boolean);
 
+// Part 1
 let two = 0;
 let three = 0;
-
 data.forEach(d => {
   let hasTwo = false;
   let hasThree = false;
@@ -23,3 +23,32 @@ data.forEach(d => {
 });
 
 console.log(two, three, two * three);
+
+// Part 2
+data.forEach((line, index) => {
+  const currentLine = line.split('');
+  const otherLines = data.slice(index + 1);
+
+  otherLines.forEach(otherLine => {
+    const lineToCompare = otherLine.split('');
+    let differentCharacters = 0;
+    let differentIndex;
+
+    currentLine.forEach((_, index) => {
+      if (currentLine[index] != lineToCompare[index]) {
+        differentCharacters++;
+        differentIndex = index;
+      }
+    });
+    if (differentCharacters < 2) {
+      const matchWithoutDifferentCharacter = line
+        .split('')
+        .map((letter, index) => {
+          return index === differentIndex ? '' : letter;
+        })
+        .join('');
+
+      console.log(matchWithoutDifferentCharacter);
+    }
+  });
+});
